24fc6c68f91e29a3eef961231560715f6038e24f
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-07-18 13:02:35 +0300

    files

--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,10 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
+// class Comment extends React.Component{
+//   render(){
+//     return <div>Naturally, I agree with this article.</div>
+//   }
+// }
+
 export default Comment;
